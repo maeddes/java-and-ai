f17223319abf2b464988ddee93747e4c466cead7
Matthias Haeussler <maeddes@gmail.com>
2024-09-13 16:56:07 +0200

    Spring AI working code

--- spring-ai-ollama/src/main/java/com/example/ollama/OllamaApplication.java
+++ spring-ai-ollama/src/main/java/com/example/ollama/OllamaApplication.java
@@ -1,22 +1,16 @@
 package com.example.ollama;
 
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
-
-import reactor.core.publisher.Flux;
-
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Map;
-import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -58,7 +52,7 @@ public class OllamaApplication {
 	}
 
 	@GetMapping("/movie")
-    public Map generate() {
+    public Map<String,String> generate() {
 
 		int rand = (new Random().nextInt(100))+1920;
 
@@ -78,7 +72,7 @@ public class OllamaApplication {
     }
 
     @GetMapping("/ai/generate")
-    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         System.out.println(message);
 		return Map.of("generation", chatModel.call(message));
     }
--- spring-ai-openai/src/main/java/io/nvtc/spring_ai_openai/SpringAiOpenaiApplication.java
+++ spring-ai-openai/src/main/java/io/nvtc/spring_ai_openai/SpringAiOpenaiApplication.java
@@ -1,12 +1,26 @@
 package io.nvtc.spring_ai_openai;
 
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringAiOpenaiApplication {
 
-	
+	@Autowired
+	OpenAiChatModel model;
+
+
+	@GetMapping
+	public String sayHello(){
+
+		String prompt = "Hello. Who are you?";
+
+		return model.call(prompt);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiOpenaiApplication.class, args);
