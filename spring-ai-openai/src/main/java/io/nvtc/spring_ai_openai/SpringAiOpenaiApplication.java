e7398e4cfcfd1f11132d01eb13d2985c5d1ffb97
Matthias Haeussler <maeddes@gmail.com>
2024-09-16 14:46:16 +0200

    initial prompt

--- spring-ai-openai/src/main/java/io/nvtc/spring_ai_openai/SpringAiOpenaiApplication.java
+++ spring-ai-openai/src/main/java/io/nvtc/spring_ai_openai/SpringAiOpenaiApplication.java
@@ -5,15 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAiOpenaiApplication {
 
 	@Autowired
 	OpenAiChatModel model;
 
 
-	@GetMapping
+	@GetMapping("/")
 	public String sayHello(){
 
 		String prompt = "Hello. Who are you?";
